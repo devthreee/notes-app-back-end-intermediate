06df4411d6a39aaf9a6bc6dd9bdc5d525324077a
tri abadi <triabadi@tris-MacBook-Air-2.local>
2023-06-13 11:32:33 +0700

    restructure with hapi plugin

---
+++ src/api/notes/routes.js
@@ -0,0 +1,63 @@
+// const { deleteNoteByIdHandler } = require('./handler');
+// const { editNoteByIdHandler } = require('./handler');
+// const { getNoteByIdHandler } = require('./handler');
+// const { addNoteHandler, getAllNotesHandler } = require('./handler');
+
+// const routes = [
+//   {
+//     method: 'POST',
+//     path: '/notes',
+//     handler: addNoteHandler,
+//   },
+//   {
+//     method: 'GET',
+//     path: '/notes',
+//     handler: getAllNotesHandler,
+//   },
+//   {
+//     method: 'GET',
+//     path: '/notes/{id}',
+//     handler: getNoteByIdHandler,
+//   },
+//   {
+//     method: 'PUT',
+//     path: '/notes/{id}',
+//     handler: editNoteByIdHandler,
+//   },
+//   {
+//     method: 'DELETE',
+//     path: '/notes/{id}',
+//     handler: deleteNoteByIdHandler,
+//   },
+// ];
+
+// module.exports = routes;
+
+const routes = (handler) => [
+  {
+    method: 'POST',
+    path: '/notes',
+    handler: handler.postNoteHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes',
+    handler: handler.getNotesHandler,
+  },
+  {
+    method: 'GET',
+    path: '/notes/{id}',
+    handler: handler.getNoteByIdHandler,
+  },
+  {
+    method: 'PUT',
+    path: '/notes/{id}',
+    handler: handler.putNoteByIdHandler,
+  },
+  {
+    method: 'DELETE',
+    path: '/notes/{id}',
+    handler: handler.deleteNoteByIdHandler,
+  },
+];
+module.exports = routes;
